c6226ffc9113467ff261e5fece243ea3bec0de41
metalx1000 <git@filmsbykris.com>
2017-10-24 18:51:29 -0400

    Front and back should work now

--- suncost_credit_union/suncoast_2014.sh
+++ suncost_credit_union/suncoast_2014.sh
@@ -19,29 +19,91 @@ read memnum
 
 ddate=`date +%m-%d-%Y`
 
-echo -n "Check Amount: "
-read chamt
+echo -n "Check#1: $"
+read check1
+if [ "$check1" = "" ]
+then
+  check1=0.00
+fi
 
-echo -n "Total Check: "
-read checkd
+echo -n "Check#2: $"
+read check2
+if [ "$check2" = "" ]
+then
+  check2=0.00
+fi
 
-echo -n "Total Cash: "
+echo -n "Check#3: $"
+read check3
+if [ "$check3" = "" ]
+then
+  check3=0.00
+fi
+
+echo -n "Check#4: $"
+read check4
+if [ "$check4" = "" ]
+then
+  check4=0.00
+fi
+
+echo -n "Check#5: $"
+read check5
+if [ "$check5" = "" ]
+then
+  check5=0.00
+fi
+
+echo -n "Check#6: $"
+read check6
+if [ "$check6" = "" ]
+then
+  check6=0.00
+fi
+
+echo -n "Check#7: $"
+read check7
+if [ "$check7" = "" ]
+then
+  check7=0.00
+fi
+
+checkd="$(echo "$check1 + $check2 + $check3 + $check4 + $check5 + $check6 + $check7"|bc)"
+
+echo -n "Total Cash: $"
 read cashd
+if [ "$cashd" = "" ]
+then
+  cashd=0.00
+fi
 
-echo -n "Total Deposite: "
-read dtotal
+#echo -n "Total Deposite: $"
+#read dtotal
+dtotal="$(echo "$checkd + $cashd"|bc)"
 
-echo -n "Cash Back: "
+echo -n "Cash Back: $"
 read cashb
 
+#edit front
 sed -i "s/nname/$nname/" $tmpfile
 sed -i "s/memnum/$memnum/" $tmpfile
 sed -i "s/ddate/$ddate/" $tmpfile
-sed -i "s/chamt/$chamt/" $tmpfile
+#sed -i "s/chamt/$chamt/" $tmpfile
+sed -i "s/chamt/$dtotal/" $tmpfile
 sed -i "s/cashd/$cashd/" $tmpfile
 sed -i "s/checkd/$checkd/" $tmpfile
 sed -i "s/dtotal/$dtotal/" $tmpfile
 sed -i "s/cashb/$cashb/" $tmpfile
+
+#edit back
+sed -i "s/check#1/$check1/" $tmpfile2
+sed -i "s/check#2/$check2/" $tmpfile2
+sed -i "s/check#3/$check3/" $tmpfile2
+sed -i "s/check#4/$check4/" $tmpfile2
+sed -i "s/check#5/$check5/" $tmpfile2
+sed -i "s/check#6/$check6/" $tmpfile2
+sed -i "s/check#7/$check7/" $tmpfile2
+sed -i "s/checkamt/$checkd/" $tmpfile2
 
 xdg-open $tmpfile2
 xdg-open $tmpfile
